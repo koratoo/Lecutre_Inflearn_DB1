ad505b3acf20b5b85f844cb00ccc93ef92aa01a5
koratoo <ssyk2@naver.com>
2023-11-04 20:39:38 +0900

    initial commit

---
+++ jdbc/src/test/java/db/jdbc/JdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package db.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
