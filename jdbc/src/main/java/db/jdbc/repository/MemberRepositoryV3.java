adab1e7065f50c0b91cb8c6c92285da04abc3907
koratoo <ssyk2@naver.com>
2023-11-05 17:35:55 +0900

    트랜잭션 동기화를 사용하려면 주의사항!
    강의 초입 부분

--- jdbc/src/main/java/db/jdbc/repository/MemberRepositoryV3.java
+++ jdbc/src/main/java/db/jdbc/repository/MemberRepositoryV3.java
@@ -1,10 +1,14 @@
 package db.jdbc.repository;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.jdbc.support.JdbcUtils;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * 트랜잭션 - 트랜잭션 매니저
@@ -19,7 +23,16 @@ public class MemberRepositoryV3 {
     public MemberRepositoryV3(DataSource dataSource){
         this.dataSource=dataSource;
     }
+
+    private void close(Connection con, Statement stmt, ResultSet rs){
+        JdbcUtils.closeResultSet(rs);
+        JdbcUtils.closeStatement(stmt);
+        //주의! 트랜잭션 동기화를 사용하려면 DataSourceRealease를 사용해야한다.
+        DataSourceUtils.releaseConnection(con,dataSource);
+//        JdbcUtils.closeConnection(con);
+    }
     private Connection getConnection() throws SQLException{
+        //주의! 트랜잭션 동기화를 사용하려면 DataSourceUtils를 사용해야 한다.
         Connection con = dataSource.getConnection();
         log.info("get connectinon={}, class={}",con,con.getClass());
         return con;
