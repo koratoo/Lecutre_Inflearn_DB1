e7fe4cc9a9f6b52a26d376f2f820608ddcf72904
koratoo <ssyk2@naver.com>
2023-11-05 22:04:26 +0900

    MemberRepositoryV0 작성

--- jdbc/src/main/java/db/jdbc/connection/DBConnectionUtil.java
+++ jdbc/src/main/java/db/jdbc/connection/DBConnectionUtil.java
@@ -1,16 +1,23 @@
 package db.jdbc.connection;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 import static db.jdbc.connection.ConnectionConst.*;
 
+@Slf4j
 public class DBConnectionUtil {
 
-    public static Connection getConnection() throws SQLException {
-        Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-
-        return connection;
+    public static Connection getConnection()  {
+        try {
+            Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            log.info("get connection={}, class={}",connection,connection.getClass());
+            return connection;
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
     }
 }
